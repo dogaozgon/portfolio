eca64411c1c20018b4607270d8de417ac97ffb55
Doga Ozgon <74891794+dogaozgon@users.noreply.github.com>
2025-10-11 13:13:59 +0000

    updated account view numbers

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -9,7 +9,7 @@ import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
 	{ number: 1200, text: "Followers On LinkedIn" },
-	{ number: 4000, text: "Subscribers on YouTube" },
+	{ number: 4100, text: "Subscribers on YouTube" },
 	{ number: 1800, text: "Followers On TikTok" },
 	{ number: "600K", text: "Social Media Video Views" },
 	{ number: "60K", text: "Article Views On Medium" },
